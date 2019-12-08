6fcf01d6e06e0f87e817b9677558d4172b5a8be2
sagar mochi <chouhansagar131@gmail.com>
2019-12-08 19:05:06 +0530

    quest 4 solved

---
+++ assignment1/code4.py
@@ -0,0 +1,39 @@
+'''
+4.How do you check if a given String contains valid parentheses? Given a string containing just the characters '(', ')', '{', '}', '[' and ']', 
+write a program in python to check if the input string is valid. The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+Function Name : check_parentheses Input : str Output : True/False
+'''
+
+
+def check_parentheses(s):
+    ans = False
+    stack = []
+    lst = [char for char in s]
+
+    for i in range(len(lst)):
+        if lst[i] == '(':
+            stack.append(lst[i])
+        if lst[i] == '{':
+            stack.append(lst[i])
+        if lst[i] == '[':
+            stack.append(lst[i])
+
+        
+        if stack[len(stack)-1] == '(' and lst[i] == ')':
+            stack.pop()
+        elif stack[len(stack)-1] == '[' and lst[i] == ']':
+            stack.pop()
+        elif stack[len(stack)-1] == '{' and lst[i] == '}':
+            stack.pop()
+        else:
+            continue
+
+    if not stack:
+        ans = True
+            
+    return ans
+
+
+k = input("Enter string:")
+ans=check_parentheses(k)
+print(ans)
