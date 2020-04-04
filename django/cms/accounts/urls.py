ed8374db42fe6ad9b0d3cb4fba49995319d23c5a
sagar mochi <chouhansagar131@gmail.com>
2020-04-04 16:22:14 +0530

    user signup feature added

--- django/cms/accounts/urls.py
+++ django/cms/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path,include
 from django.contrib.auth import urls
+from accounts.views import UserCreateView
+
 
 urlpatterns = [
     path('',include(urls)),
+    path('signup',UserCreateView.as_view()),
 ]
