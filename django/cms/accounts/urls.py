906f04d26f0874d6b68abe09dfb64b0748447bc4
sagar mochi <chouhansagar131@gmail.com>
2020-04-07 00:57:46 +0530

    file structure changed

--- django/cms/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from django.contrib.auth import urls
-from accounts.views import UserCreateView
-
-
-urlpatterns = [
-    path('',include(urls)),
-    path('signup',UserCreateView.as_view()),
-]
